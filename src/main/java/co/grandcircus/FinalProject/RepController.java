82418b4d0f8647a472ab2efca5d8a83d1dbf60aa
Sasi K <kbsasikarthik@gmail.com>
2018-09-10 14:52:33 -0400

    state and city selection working

--- src/main/java/co/grandcircus/FinalProject/RepController.java
+++ src/main/java/co/grandcircus/FinalProject/RepController.java
@@ -22,57 +22,42 @@ import co.grandcircus.FinalProject.entity.RepResult;
 @Controller
 public class RepController {
 	
-	@Autowired
-	IncidentDao incidentDao;
-	
-	@RequestMapping("/")
-	public ModelAndView showhome() {
-		ModelAndView mav = new ModelAndView("home");
-		mav.addObject("states", incidentDao.getStates());
-		return mav;
-	}
-	
-	@RequestMapping("/selectCity")
-	public ModelAndView showCities(
-			@RequestParam("state") String state) {
-		ModelAndView mav = new ModelAndView("redirect:/");
-		mav.addObject("cities", incidentDao.getCities(state));
-		return mav;
-	}
 
-	
-	@RequestMapping("/repbyzip")
-	public ModelAndView showreps(@RequestParam("zipcode") Integer zipcode, @RequestParam("state") String state) {
-		
-		System.out.println(zipcode+","+state);
-		ModelAndView mav = new ModelAndView("show-rep");
-//		String states = "Michigan";
-		RestTemplate restTemplate = new RestTemplate();
-				
-		List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
-		MappingJackson2HttpMessageConverter msgconverter = new MappingJackson2HttpMessageConverter();
-		List<MediaType> mediaTypes = new ArrayList<MediaType>();
-		mediaTypes.add(MediaType.TEXT_HTML);
-		msgconverter.setSupportedMediaTypes(mediaTypes);
-		messageConverters.add(msgconverter);
-		restTemplate.setMessageConverters(messageConverters);
-		
-
-		String url = "https://whoismyrepresentative.com/getall_mems.php?zip="+zipcode+"&output=json";
-
-//		Integer zip = zipcode;
-		ResponseEntity<RepResult> response = restTemplate.exchange(url, HttpMethod.GET, null, RepResult.class);
-		System.out.println(response);
-		RepResult result = response.getBody();
-		System.out.println(result.getResults());
-		List<Incident> incidents = incidentDao.byState(state);
-		System.out.println("In controller"+incidents);
-		mav.addObject("incidents", incidents);
-		mav.addObject("zipcode", zipcode);
-		mav.addObject("represult", result.getResults());
-		return mav;
-		
-	}
+//	
+//	@RequestMapping("/repbyzip")
+//	public ModelAndView showreps(@RequestParam("zipcode") Integer zipcode, @RequestParam("state") String state) {
+////	public ModelAndView showdetails(@RequestParam("state") String state) {	
+////		System.out.println(zipcode+","+state);
+//		ModelAndView mav = new ModelAndView("show-rep");
+////		String states = "Michigan";
+//		RestTemplate restTemplate = new RestTemplate();
+//				
+//		List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
+//		MappingJackson2HttpMessageConverter msgconverter = new MappingJackson2HttpMessageConverter();
+//		List<MediaType> mediaTypes = new ArrayList<MediaType>();
+//		mediaTypes.add(MediaType.TEXT_HTML);
+//		msgconverter.setSupportedMediaTypes(mediaTypes);
+//		messageConverters.add(msgconverter);
+//		restTemplate.setMessageConverters(messageConverters);
+////		
+//
+//		String url = "https://whoismyrepresentative.com/getall_mems.php?zip="+zipcode+"&output=json";
+////		String url = "https://openstates.org/api/v1/legislators/geo/?lat="+lat+"&long="+lan+"&apikey=239038b3-30a9-4f08-a887-90bae55dd133";
+////		opnstates.org apikey - 239038b3-30a9-4f08-a887-90bae55dd133
+//
+////		Integer zip = zipcode;
+//		ResponseEntity<RepResult> response = restTemplate.exchange(url, HttpMethod.GET, null, RepResult.class);
+//		System.out.println(response);
+//		RepResult result = response.getBody();
+//		System.out.println(result.getResults());
+////		List<Incident> incidents = incidentDao.byState(state);
+//		System.out.println("In controller"+incidents);
+//		mav.addObject("incidents", incidents);
+//		mav.addObject("zipcode", zipcode);
+//		mav.addObject("represult", result.getResults());
+//		return mav;
+//		
+//	}
 	
 
 }
--- src/main/webapp/WEB-INF/views/NewFile.jsp
+++ src/main/webapp/WEB-INF/views/NewFile.jsp
@@ -1,12 +1,36 @@
+<%@page import="java.util.Random"%>
+<%@page import="java.util.Arrays"%>
+
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<title>Home page</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.3/journal/bootstrap.min.css" />
+<link rel="stylesheet" href="/style.css" />
+
 </head>
 <body>
 
+
+<form method ="post" action="/listresults">
+		 <div class="form-group">
+		  	<label class="sr-only" for="city">City</label>
+			<select class="form-control mb-2 mr-sm-2" id="city" name="city">
+			<option value="">City</option>
+		  	<c:forEach items="${ cities }" var="city">
+		  		<option <c:if test="${ city eq param.city_or_county }">selected</c:if>>${ city }</option>
+		  	</c:forEach>
+		  	</select>
+		 </div>
+		<button action = "submit">Submit</button>
+	</form>
 </body>
 </html>
