d1f331a3666b59972770550b994e9529b71956a6
cameronindetroit <cameronindetroit@gmail.com>
2018-09-19 11:20:06 -0400

    socialDao merge

--- src/main/java/co/grandcircus/FinalProject/dao/ConnectionDao.java
+++ src/main/java/co/grandcircus/FinalProject/dao/ConnectionDao.java
@@ -3,6 +3,7 @@ package co.grandcircus.FinalProject.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
@@ -13,10 +14,13 @@ import co.grandcircus.FinalProject.entity.Connection;
 @Repository
 public class ConnectionDao {
 
+	@PersistenceContext
 	private EntityManager em;
 
 	public void create(Connection connection) {
+
 		em.persist(connection);
+
 	}
 
 	public void update(Connection connection) {
